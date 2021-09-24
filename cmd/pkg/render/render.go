c9fd000160203646f9ca43842547b6a18677cffa
tirzasrwn <tirzasrwn@gmail.com>
2021-09-24 11:07:49 +0800

    feat: Optimizing template cache using an application config.

--- cmd/pkg/render/render.go
+++ cmd/pkg/render/render.go
@@ -1,16 +1,67 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"net/http"
+	"path/filepath"
 	"text/template"
+
+	"github.com/tirzasrwn/gocourse/cmd/pkg/config"
 )
 
+var fuction = template.FuncMap{}
+
+var app *config.AppConfig
+
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parseTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error parsing template:", err)
-		return
+	var tc map[string]*template.Template
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
 	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Could not get template from template cache.")
+	}
+	buf := new(bytes.Buffer)
+	_ = t.Execute(buf, nil)
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser: ", err)
+	}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./templates/*.page.html")
+	if err != nil {
+		return myCache, err
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(fuction).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+		matches, err := filepath.Glob("./templates/*.layout.html")
+		if err != nil {
+			return myCache, err
+		}
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.html")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/tirzasrwn/gocourse/cmd/pkg/config"
 	"github.com/tirzasrwn/gocourse/cmd/pkg/handlers"
+	"github.com/tirzasrwn/gocourse/cmd/pkg/render"
 )
 
 const portNumber = ":4545"
 
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	var app config.AppConfig
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache.")
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+	render.NewTemplates(&app)
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Printf("Starting application on localhost%s.\n", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
